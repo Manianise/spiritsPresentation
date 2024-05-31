14eb55d18bb2929593a3d58409713a60b396ed7c
Mechameleon <41825442+Manianise@users.noreply.github.com>
2024-05-31 10:07:00 +0200

    tests

--- dist/assets/VideoLoop-8613aafd.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-3d20fc75.js";function i({path:t}){return o("video",{src:t,autoPlay:!0,muted:!0,loop:!0,style:{objectFit:"contain",maxWidth:"inherit",width:"100%"}})}export{i as default};
